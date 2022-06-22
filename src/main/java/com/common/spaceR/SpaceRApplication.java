f6ba1a1b80b65a7b9831c99f2b33ad1300e2e993
suebinjang <toktoksbtn@gmail.com>
2022-06-22 19:57:32 +0900

    chore: 초기 설정

---
+++ src/main/java/com/common/spaceR/SpaceRApplication.java
@@ -0,0 +1,13 @@
+package com.common.spaceR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/common/spaceR/SpaceRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.spaceR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
